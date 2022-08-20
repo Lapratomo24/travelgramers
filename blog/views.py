a8b083566f02f4871d7c948c4c76562458d72a79
Lapratomo24 <lapratomo24@yahoo.com>
2022-08-20 22:42:03 +0000

    setup and connect homepage to post detail page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import View, ListView
 from django.views.generic.edit import UpdateView, DeleteView
 from .models import Post
@@ -14,3 +14,26 @@ class PostList(ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class PostDetail(View):
+    """
+    Class to view the content of a selected post
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            }
+        )
