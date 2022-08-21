344daba487de72c68577178d6594f49ade5a7c89
Lapratomo24 <lapratomo24@yahoo.com>
2022-08-21 21:21:55 +0000

    add like and unlike functionality

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views.generic import View, ListView
 from django.views.generic.edit import UpdateView, DeleteView
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -72,3 +73,18 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             }
         )
+
+
+class PostLike(View):
+    """
+    Class to give users the ability to like or unlike posts
+    """
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else: 
+            post.likes.add(request.user)
+        
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
