37078d7f4010ef21b760eac69c4fd0ec010d4abe
Lapratomo24 <lapratomo24@yahoo.com>
2022-08-21 16:23:05 +0000

    setup skeleton signup, login, and logout urls

--- travelgramers/settings.py
+++ travelgramers/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['travelgramers-blog.herokuapp.com', 'localhost']
-
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Application definition
 
@@ -40,12 +40,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
