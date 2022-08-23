831fd82a18b47e27b89b346d70ac705b5c619276
Lapratomo24 <lapratomo24@yahoo.com>
2022-08-23 18:03:59 +0000

    site crashed for reasons unknown; will fix with mentor help

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('create/', views.CreatePost.as_view(), name='create'),
-    path('slug:slug>', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
