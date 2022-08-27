7da921033a4542057a37d092456e61f335bdc237
Lapratomo24 <lapratomo24@yahoo.com>
2022-08-27 09:34:40 +0000

    add django/python test for views.py

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,16 +1,29 @@
+"""Create test for forms.py"""
+
 from django.test import TestCase
 from .forms import ShareForm
 
 # Create your tests here.
 
-class TestShareForm(TestCase):
 
+class TestShareForm(TestCase):
+    """
+    Class to test forms.py
+    """
     def test_title_is_required(self):
+        """
+        Method to test error message when title is empty
+        """
         form = ShareForm({'title': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('title', form.errors.keys())
         self.assertEqual(form.errors['title'][0], 'This field is required.')
-    
+
     def test_fields_are_explicit_in_form_metaclass(self):
+        """
+        Method to test whether the fields displayed to users are as defined
+        """
         form = ShareForm()
-        self.assertEqual(form.Meta.fields, ["title", "featured_image", "description", "attractions"])
+        self.assertEqual(
+            form.Meta.fields, [
+                "title", "featured_image", "description", "attractions"])
--- blog/test_views.py
+++ blog/test_views.py
@@ -1,3 +1,26 @@
+"""Create test for views.py"""
+
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestViews(TestCase):
+    """
+    Class to test views.py
+    """
+    def test_get_paginated_list(self):
+        """
+        Method to test whether the homepage can be displayed
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_get_about_page(self):
+        """
+        Method to test whether the about page can be displayed
+        """
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
